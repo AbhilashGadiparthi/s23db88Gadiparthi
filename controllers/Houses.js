797c92d8026a055b316218c86e9f52c2c9d701cf
AbhilashGadiparthi <143052901+AbhilashGadiparthi@users.noreply.github.com>
2023-11-15 10:11:11 -0600

    commit

--- controllers/Houses.js
+++ controllers/Houses.js
@@ -77,3 +77,24 @@ exports.Houses_detail = async function(req, res) {
     res.send(`{"error": document for id ${req.params.id} not found`);
     }
    };
+
+   // Handle Costume update form on PUT.
+exports.Houses_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body
+   ${JSON.stringify(req.body)}`)
+    try {
+    let toUpdate = await Houses.findById( req.params.id)
+    // Do updates of properties
+    if(req.body.house_name)
+    toUpdate.house_name = req.body.house_name;
+    if(req.body.house_size ) toUpdate.house_size = req.body.house_size;
+    if(req.body.house_price) toUpdate.house_price = req.body.house_price;
+    let result = await toUpdate.save();
+    console.log("Sucess " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": ${err}: Update for id ${req.params.id}
+   failed`);
+    }
+   };
--- routes/resource.js
+++ routes/resource.js
@@ -19,23 +19,4 @@ router.get('/Houses/:id', Houses_controller.Houses_detail);
 router.get('/Houses', Houses_controller.Houses_list);
 module.exports = router;
 
-// Handle Costume update form on PUT.
-exports.Houses_update_put = async function(req, res) {
- console.log(`update on id ${req.params.id} with body
-${JSON.stringify(req.body)}`)
- try {
- let toUpdate = await Houses.findById( req.params.id)
- // Do updates of properties
- if(req.body.costume_type)
- toUpdate.Houses_type = req.body.Houses_type;
- if(req.body.cost) toUpdate.cost = req.body.cost;
- if(req.body.size) toUpdate.size = req.body.size;
- let result = await toUpdate.save();
- console.log("Sucess " + result)
- res.send(result)
- } catch (err) {
- res.status(500)
- res.send(`{"error": ${err}: Update for id ${req.params.id}
-failed`);
- }
-};
+
